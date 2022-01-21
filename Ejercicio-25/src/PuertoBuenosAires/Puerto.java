9e2a296928374d215b093136c9600e533117af78
cristaldoplf <cristaldoplf@gmail.com>
2022-01-21 10:29:20 -0300

    Clases del ejercicio 25 terminadas

---
+++ Ejercicio-25/src/PuertoBuenosAires/Puerto.java
@@ -0,0 +1,24 @@
+package PuertoBuenosAires;
+
+import java.util.ArrayList;
+
+public class Puerto {
+    private String nombre;
+    private ArrayList<Contenedor> contenedores = new ArrayList<>();
+
+    public Puerto(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void ingresarContenedor(Contenedor cont) {
+        contenedores.add(cont);
+    }
+
+    public void mostrarContenedores() {
+        contenedores.sort(null);
+        for (Contenedor cont : contenedores) ;
+        {
+            System.out.println(contenedores.toString());
+        }
+    }
+}
