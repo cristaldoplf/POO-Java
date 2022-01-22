093a70bd9ae8ba0b3182f6c9cc4e9328f34b07a6
cristaldoplf <cristaldoplf@gmail.com>
2022-01-21 21:28:20 -0300

    trabajo 27 terminado

--- Ejercicio-27-Excepciones/src/Clinic/Main.java
+++ Ejercicio-27-Excepciones/src/Clinic/Main.java
@@ -6,8 +6,8 @@ public class Main {
 
     public static void main(String[] args) throws PacienteException {
         try{
-            Paciente paciente=new Paciente("Juan","Perez","12345",new Date());
-            paciente.darAlta(new Date(2001,10,10));
+            Paciente paciente=new Paciente("Juan","Perez","12345",new Date(2021-1900, 5-1,10));
+            paciente.darAlta(new Date(2022-1950,12-1,10));
         }catch (PacienteException e){
             e.printStackTrace();
         }
